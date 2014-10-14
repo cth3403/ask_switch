f441f8d70f664445e07ba18e1a5bab45d05bd8e6
Joe Schulkins <jschulkins@gmail.com>
2014-10-14 10:27:56 +0100

    initial commit

---
+++ ask_switch.php
@@ -0,0 +1,86 @@
+<?php
+
+
+$jd=cal_to_jd(CAL_GREGORIAN,date("m"),date("d"),date("Y"));
+$dayW = jddayofweek($jd,0);
+
+$dayArr= array(6,0);
+
+// check to see whether there are any closed days set
+if (sizeof($dayArr) > 0) {
+
+foreach ($dayArr as $key => $value) {
+	if($value == $dayW){
+		// set the chat as closed
+	}
+	else{
+		return;
+	}
+}
+
+}
+
+
+
+$switch_var = "11:50";
+$switch_time = strtotime($switch_var);
+$real_time = strtotime("now");
+
+if($real_time >= $switch_time){
+
+/*
+echo "switch";
+echo "<br /><br />";
+echo $switch_time;
+echo "<br /><br />";
+echo $real_time;
+echo "<br /><br />";
+
+echo "QuestionPoint";
+*/
+
+echo '<style>
+#qpchatwidget{
+    border-color: #FF9900;
+
+.qp_institution_name_bar{
+    background-color: #FF9900;
+
+</style>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js" type="text/javascript" ></script>
+
+<!-- If using qwidget size "fill", make sure that the minimum height for the parent element of qwidget container on this page is 230 px and the minimum width is 160px.-->
+<!-- NOTE - After pasting the code to your page, check it to ensure it does not contain any text formatting such as smart or curly quotation marks.-->
+<!-- IMPORTANT - If using multiple chat widgets on the same page: Please incrementally change the "qwidgetno" attribute on the div and script elements for each additional qwidget on the same page.-->
+<!-- Beginning of QuestionPoint qwidget code. -->
+<div id="questionpoint.chatwidget" qwidgetno="1" ></div>
+<script id="questionpoint.bootstrap" qwidgetno="1" type="text/javascript" src="http://www.questionpoint.org/crs/qwidgetV4/js/qwidget.bootstrap.js?langcode=1&instid=13842&skin=blue&size=standard" charset="utf-8">//<noscript>Please enable javascript to chat with librarians online</noscript></script>';
+
+
+}
+else{
+
+/*
+echo "no switch";
+echo "<br /><br />";
+echo $switch_time;
+echo "<br /><br />";
+echo $real_time;
+echo "<br /><br />";
+
+
+echo "Libchat<br />";
+*/
+
+echo '<div id="libchat_inline_widget"></div> <script> var libchat_1413effb6ce639d15c0f7b6e66d2acd7 = { iid:447, key:"9674f955eb730bc", width:"240" }</script> <script type="text/javascript" src="//liv.ac.uk.libanswers.com/load_chat.php?hash=1413effb6ce639d15c0f7b6e66d2acd7&options=libchat_1413effb6ce639d15c0f7b6e66d2acd7"></script>';
+
+}
+
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Switch library chat</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+  </head>
+
+  <body>
+
+
+
+
+
+
+
+
+
+  	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="//code.jquery.com/ui/1.11.1/jquery-ui.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>  
+  </body>
+</html>
